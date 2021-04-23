903a1efaba85c6cd2f5f04c45b8bf5a1511b821d
MuhammadKhosravi <mohamad138121@gmail.com>
2021-04-23 20:37:04 +0430

    adding functions from uml

--- src/main/java/module/User.java
+++ src/main/java/module/User.java
@@ -6,30 +6,78 @@ public class User {
     private String nickname;
     private int money;
 
-    public String getPassword() {
-        return password;
+    public User(username:String, password:String, nickname:String) {
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public static User getUserByUsername(String username) {
     }
 
-    public int getMoney() {
-        return money;
+    public void setPassowrd(String newPassword) {
     }
 
-    public String getUsername() {
-        return username;
+    public void getPassword() {
+    }
+
+    public String getMoney() {
     }
 
     public void setMoney(int money) {
-        this.money = money;
     }
 
-    public String getNickname() {
-        return nickname;
+    public Board getBoard() {
     }
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
+
+    public void setBoard(Board board) {
     }
+
+    public Board setUsername(String username) {
+    }
+
+    public void getUsername() {
+    }
+
+    public String setNichname(String nichname) {
+    }
+
+    public void getNichname() {
+    }
+
+    public String setScore(int score) {
+    }
+
+    public void getScore() {
+    }
+
+    public int setActiveDeck(Deck deck) {
+    }
+
+    public void getActiveDeck() {
+    }
+
+    public Deck setSideDeck(Deck deck) {
+    }
+
+    public void getSideDeck() {
+    }
+
+    public Deck getDecks() {
+    }
+
+    public Arraylist<Deck> addDeck(deck:Deck) {
+    }
+
+    public void removeDeck(deck:Deck) {
+    }
+
+    public void addCard(card:Card) {
+    }
+
+    public void remvoeCard(card:Card) {
+    }
+
+    public void increaseMoney(amount:int){}
+
+    public void increaseScore(amount:int){}
+
+    public boolean doesUserExist(username:String){}
 }
