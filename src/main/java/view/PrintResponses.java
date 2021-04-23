20c7221546d48ce355d2125df11dbc5857f5b85f
Farhad Esmaeilzadeh <farhadesmzdh@gmail.com>
2021-04-23 21:14:13 +0430

    adding class PrintResponses

---
+++ src/main/java/view/PrintResponses.java
@@ -0,0 +1,251 @@
+package view;
+
+public class PrintResponses {
+    public static void printInvalidFormat() {
+        System.out.println(Responses.invalidFormat);
+    }
+
+    public static void printNoLoginYet() {
+        System.out.println(Responses.noLoginYet);
+    }
+
+    public static void printMenuNavigationError() {
+        System.out.println(Responses.menuNavigationError);
+    }
+
+    public static void printSuccessfulUserCreation() {
+        System.out.println(Responses.successfulUserCreation);
+    }
+
+    public static void printNoUserExists() {
+        System.out.println(Responses.noUserExists);
+    }
+
+    public static void printWrongPasswordInLogin() {
+        System.out.println(Responses.wrongPasswordInLogin);
+    }
+
+    public static void printSuccessfulLogout() {
+        System.out.println(Responses.successfulLogout);
+    }
+
+    public static void printSuccessfulNicknameChange() {
+        System.out.println(Responses.successfulNicknameChange);
+    }
+
+    public static void printSuccessfulPasswordChange() {
+        System.out.println(Responses.successfulPasswordChange);
+    }
+
+    public static void printWrongPasswordInChange() {
+        System.out.println(Responses.wrongPasswordInChange);
+    }
+
+    public static void printEqualityOfCurrentAndNewPassword() { System.out.println(Responses.equalityOfCurrentAndNewPassword); }
+
+    public static void printSuccessfulDeckCreation() {
+        System.out.println(Responses.successfulDeckCreation);
+    }
+
+    public static void printSuccessfulDeckDeletion() {
+        System.out.println(Responses.successfulDeckDeletion);
+    }
+
+    public static void printSuccessfulDeckActivation() {
+        System.out.println(Responses.successfulDeckActivation);
+    }
+
+    public static void printSuccessfulCardAddition() {
+        System.out.println(Responses.successfulCardAddition);
+    }
+
+    public static void printSuccessfulCardRemoval() {
+        System.out.println(Responses.successfulCardRemoval);
+    }
+
+    public static void printNoCardExistToBuy() {
+        System.out.println(Responses.noCardExistToBuy);
+    }
+
+    public static void printLackOfMoney() {
+        System.out.println(Responses.lackOfMoney);
+    }
+
+    public static void printNoUserExistToPlayWith() {
+        System.out.println(Responses.noUserExistToPlayWith);
+    }
+
+    public static void printNonSupportiveRound() {
+        System.out.println(Responses.nonSupportiveRound);
+    }
+
+    public static void printInvalidSelection() {
+        System.out.println(Responses.invalidSelection);
+    }
+
+    public static void printSuccessfulCardSelection() {
+        System.out.println(Responses.successfulCardSelection);
+    }
+
+    public static void printNoCardInPosition() {
+        System.out.println(Responses.noCardInPosition);
+    }
+
+    public static void printNoCardSelected() {
+        System.out.println(Responses.noCardSelected);
+    }
+
+    public static void printSuccessfulCardDeselection() {
+        System.out.println(Responses.successfulCardDeselection);
+    }
+
+    public static void printUnableToSummonCard() {
+        System.out.println(Responses.unableToSummonCard);
+    }
+
+    public static void printSummonInWrongPhase() {
+        System.out.println(Responses.summonInWrongPhase);
+    }
+
+    public static void printFullnessOfMonsterCardZone() {
+        System.out.println(Responses.fullnessOfMonsterCardZone);
+    }
+
+    public static void printUnableToSummonInTurn() {
+        System.out.println(Responses.unableToSummonInTurn);
+    }
+
+    public static void printSuccessfulSummon() {
+        System.out.println(Responses.successfulSummon);
+    }
+
+    public static void printNoCardToTribute() {
+        System.out.println(Responses.noCardToTribute);
+    }
+
+    public static void printNoMonsterOnAddress() {
+        System.out.println(Responses.noMonsterOnAddress);
+    }
+
+    public static void printNoMonsterOnAddresses() {
+        System.out.println(Responses.noMonsterOnAddresses);
+    }
+
+    public static void printUnableToSetCard() {
+        System.out.println(Responses.unableToSetCard);
+    }
+
+    public static void printSetCardInWrongPhase() {
+        System.out.println(Responses.setCardInWrongPhase);
+    }
+
+    public static void printSuccessfulCardSetting() {
+        System.out.println(Responses.successfulCardSetting);
+    }
+
+    public static void printUnableToChangePositionOfCard() { System.out.println(Responses.unableToChangePositionOfCard); }
+
+    public static void printCardInWantedPosition() {
+        System.out.println(Responses.cardInWantedPosition);
+    }
+
+    public static void printUnableToChangePositionInTurnTwice() { System.out.println(Responses.unableToChangePositionInTurnTwice); }
+
+    public static void printChangeMonsterCardModeInWrongPhase() { System.out.println(Responses.changeMonsterCardModeInWrongPhase); }
+
+    public static void printSuccessfulMonsterCardPositionChange() { System.out.println(Responses.successfulMonsterCardPositionChange); }
+
+    public static void printUnableToFlipSummonCard() {
+        System.out.println(Responses.unableToFlipSummonCard);
+    }
+
+    public static void printFlipSummonInWrongPhase() {
+        System.out.println(Responses.flipSummonInWrongPhase);
+    }
+
+    public static void printSuccessfulFlipSummon() {
+        System.out.println(Responses.successfulFlipSummon);
+    }
+
+    public static void printUnableToAttack() {
+        System.out.println(Responses.unableToAttack);
+    }
+
+    public static void printAttackInWrongPhase() {
+        System.out.println(Responses.attackInWrongPhase);
+    }
+
+    public static void printCardAttackedBefore() {
+        System.out.println(Responses.cardAttackedBefore);
+    }
+
+    public static void printNoCardToAttackWith() {
+        System.out.println(Responses.noCardToAttackWith);
+    }
+
+    public static void printDestructionOfTwoSideCards() {
+        System.out.println(Responses.destructionOfTwoSideCards);
+    }
+
+    public static void printDestructionOfDefensePositionMonster() { System.out.println(Responses.destructionOfDefensePositionMonster); }
+
+    public static void printNoDestruction() {
+        System.out.println(Responses.noDestruction);
+    }
+
+    public static void printNonSpellCardsToActivateEffect() { System.out.println(Responses.nonSpellCardsToActivateEffect); }
+
+    public static void printUnableToActivateEffectOnTurn() { System.out.println(Responses.unableToActivateEffectOnTurn); }
+
+    public static void printUnableToActivateCardTwice() {
+        System.out.println(Responses.unableToActivateCardTwice);
+    }
+
+    public static void printFullnessOfSpellCardZone() {
+        System.out.println(Responses.fullnessOfSpellCardZone);
+    }
+
+    public static void printUnfinishedPreparationOfSpell() { System.out.println(Responses.unfinishedPreparationOfSpell); }
+
+    public static void printSuccessfulSpellActivation() {
+        System.out.println(Responses.successfulSpellActivation);
+    }
+
+    public static void printSetSpellInWrongPhase() {
+        System.out.println(Responses.setSpellInWrongPhase);
+    }
+
+    public static void printSuccessfulSpellSetting() {
+        System.out.println(Responses.successfulSpellSetting);
+    }
+
+    public static void printAskToActivateSpellOrTrap() {
+        System.out.println(Responses.askToActivateSpellOrTrap);
+    }
+
+    public static void printWrongTurnToActivateSpellOrTrap() { System.out.println(Responses.wrongTurnToActivateSpellOrTrap); }
+
+    public static void printSuccessfulSpellOrTrapActivation() { System.out.println(Responses.successfulSpellOrTrapActivation); }
+
+    public static void printUnableToRitualSummonMonster() { System.out.println(Responses.unableToRitualSummonMonster); }
+
+    public static void printEmergencyRitualSummon() {
+        System.out.println(Responses.emergencyRitualSummon);
+    }
+
+    public static void printInequalityOfLevelsOfSelectedAndRitualMonster() { System.out.println(Responses.inequalityOfLevelsOfSelectedAndRitualMonster); }
+
+    public static void printUnableToSpecialSummonMonster() { System.out.println(Responses.unableToSpecialSummonMonster); }
+
+    public static void printEmergencySpecialSummon() {
+        System.out.println(Responses.emergencySpecialSummon);
+    }
+
+    public static void printEmptinessOfGraveyard() {
+        System.out.println(Responses.emptinessOfGraveyard);
+    }
+
+    public static void printUnableToShowOpponentCard() {
+        System.out.println(Responses.unableToShowOpponentCard);
+    }
+}
