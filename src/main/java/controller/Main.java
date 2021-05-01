d32574ed8984638477501ae93a092da5420eeede
MuhammadKhosravi <mohamad138121@gmail.com>
2021-05-01 14:43:37 +0430

    fixing a bug

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -2,8 +2,7 @@ package controller;
 
 public class Main {
     public static void main(String[] args) {
-//        ProgramController programController = new ProgramController();
-//        programController.run();
-        System.out.println(DataController.getAllUsers());
+        ProgramController programController = new ProgramController();
+        programController.run();
     }
 }
