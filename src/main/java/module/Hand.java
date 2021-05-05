df14baed44c1e0ee64816acfef137dee6a2bb8fa
Farhad Esmaeilzadeh <farhadesmzdh@gmail.com>
2021-05-05 18:23:46 +0430

    Adding Class Hand

--- src/main/java/module/Hand.java
+++ src/main/java/module/Hand.java
@@ -1,5 +1,52 @@
 package module;
 
-public class Hand {
+import module.card.Card;
 
+import java.util.ArrayList;
+
+public class Hand {
+    private ArrayList<Card> cardsInHand;
+    private User handOwner;
+    private Deck deckToDraw;
+    private Boolean canDraw;
+
+    private Hand (User handOwner) {
+        this.handOwner = handOwner;
+    }
+
+    private void addCardToHand (){
+
+    }
+
+    private void removeCardFromHand () {
+
+    }
+
+    private  ArrayList<Card> getCardsInHand () {
+        return cardsInHand;
+    }
+
+    private void drawACard () {
+
+    }
+
+    private void shuffleDeck () {
+
+    }
+
+    private Card selectACard (int cardAddress) {
+
+    }
+
+    private Card findACard(Card[] cards) {
+
+    }
+
+    private void discardACard (int place) {
+
+    }
+
+    private Card selectARandomCardFromHand () {
+
+    }
 }
