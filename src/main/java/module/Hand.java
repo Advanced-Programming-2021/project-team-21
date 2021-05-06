a04d9953fef48368287b871b3a54b9a167eeab92
Farhad Esmaeilzadeh <farhadesmzdh@gmail.com>
2021-05-06 19:16:29 +0430

    Completing Class Hand

--- src/main/java/module/Hand.java
+++ src/main/java/module/Hand.java
@@ -1,55 +1,86 @@
 package module;
 
 import module.card.Card;
+import view.PrintResponses;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
 
 public class Hand {
-    private ArrayList<Card> cardsInHand;
+    private Card[] cardsInHand;
     private User handOwner;
     private Deck deckToDraw;
     private Boolean canDraw;
     private Card selectedCardInHand;
 
-    public Hand (User handOwner) {
+    {
+        selectedCardInHand = null;
+        cardsInHand = new Card[6];
+    }
+
+    public Hand(User handOwner) {
         User.getUserByUsername(handOwner.getUsername()).setHand(this);
+        deckToDraw = handOwner.getActiveDeck();
     }
 
-    public void addCardToHand (Card selectedCard){
-        cardsInHand.add(selectedCard);
+    public void addCardToHand(Card cardToAdd) {
+
     }
 
-    public void removeCardFromHand (Card selectedCard) {
-        cardsInHand.remove(selectedCard);
+    public void removeCardFromHand(int cardAddress) {
+        cardsInHand[cardAddress - 1] = null;
     }
 
-    public ArrayList<Card> getCardsInHand () {
+    public Card[] getCardsInHand() {
         return cardsInHand;
     }
 
-    public Card drawACard () {
-        return selectedCardInHand;
+    public void drawACard() {
+        if (deckToDraw.getMainDeckCards().size() == 0)
+            // ending game
+            for (int i = getCardsInHand().length - 1; i >= 0; i++) {
+                if (cardsInHand[i] == null)
+                    cardsInHand[i] = deckToDraw.getMainDeckCards().get(0);
+            }
+        System.out.println("new card added to the hand : " + deckToDraw.getMainDeckCards().get(0).getName());
+        deckToDraw.getMainDeckCards().remove(0);
     }
 
-    public void shuffleDeck () {
-        Collections.shuffle(cardsInHand);
+    public void shuffleDeck() {
+        Collections.shuffle(deckToDraw.getMainDeckCards());
     }
 
-    public void selectACard (int cardAddress) {
-        selectedCardInHand =  cardsInHand.get(cardAddress - 1);
+    public void selectACard(int cardAddress) {
+        if (cardAddress > 6)
+            PrintResponses.printInvalidSelection();
+        else if (cardsInHand[cardAddress - 1] == null)
+            PrintResponses.printNoCardInPosition();
+        else {
+            selectedCardInHand = cardsInHand[cardAddress - 1];
+            PrintResponses.printSuccessfulCardSelection();
+        }
     }
 
-    //public Card findACard(Card[] cards) {
-    //    return null;
-   // }
-
-    public void discardACard (int place) {
-        selectedCardInHand = null;
+    public void deselectACard() {
+        if (selectedCardInHand == null)
+            PrintResponses.printNoCardSelected();
+        else {
+            selectedCardInHand = null;
+            PrintResponses.printSuccessfulCardDeselection();
+        }
     }
 
-    public Card selectARandomCardFromHand () {
-        return cardsInHand.get(new Random().nextInt(cardsInHand.size() - 1));
+    public void discardACard(int place) {
+
+    }
+
+    public Card selectARandomCardFromHand() {
+        ArrayList<Integer> randomizeCards = new ArrayList<>();
+        for (int i = 0; i < cardsInHand.length; i++)
+            if (cardsInHand[i] != null)
+                randomizeCards.add(i);
+        Collections.shuffle(randomizeCards);
+        return cardsInHand[randomizeCards.get(0)];
     }
 }
