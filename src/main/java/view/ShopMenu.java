642d39fd63b917ecda974e8e4cf3f5c0f5b03e55
Farhad Esmaeilzadeh <farhadesmzdh@gmail.com>
2021-07-06 17:36:42 +0430

    shop menu fix bugs(f*** cheat)

--- src/main/java/view/ShopMenu.java
+++ src/main/java/view/ShopMenu.java
@@ -25,21 +25,26 @@ import model.card.Card;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.regex.Matcher;
 
 public class ShopMenu implements Menuable {
     public static ArrayList<String> cardsName;
     private Stage stageCheat = new Stage();
     public TextField moneyCheat = new TextField();
+    private HashMap<String, Label> countToUpdate = new HashMap<>();
+    private static HashMap<String, Button> buttonToUpdate = new HashMap<>();
 
     public void showMenu() throws IOException {
         ProgramController.createNewScene(getClass().getResource("/FXMLs/shopMenu.fxml"));
-        ((Label) ProgramController.currentScene.lookup("#coin")).setText(String.valueOf(ProgramController.userInGame.getCoins()));
+        ((Label) ProgramController.currentScene.lookup("#coin")).setText("Coins : " + String.valueOf(ProgramController.userInGame.getCoins()));
         cardsName = new ArrayList<>(DataController.getAllCards().keySet());
         Collections.sort(cardsName);
         ListView<VBox> listView = (ListView<VBox>) ProgramController.currentScene.lookup("#showCards");
+        listView.setStyle("-fx-background-radius: 10;");
         for (String name : cardsName) {
             VBox vBox = new VBox();
+            vBox.setPrefWidth(120);
             vBox.setStyle("-fx-background-color: gray;");
             vBox.setAlignment(Pos.CENTER);
             vBox.setSpacing(20);
@@ -50,13 +55,28 @@ public class ShopMenu implements Menuable {
             Button button = new Button("Buy");
             button.setPrefWidth(100);
             button.setOnMouseClicked(event -> buy(DataController.getAllCards().get(name)));
-            if (ProgramController.userInGame.getCoins() < Card.getCardByName(name).getPrice())
-                button.setDisable(true);
+            button.getStyleClass().add("buttonEntrance");
+            buttonToUpdate.put(name, button);
             vBox.getChildren().add(button);
             listView.getItems().add(vBox);
         }
+        updateButtons();
         ProgramController.stage.show();
     }
+
+    private void updateButtons() {
+        for (String name : cardsName) {
+            if (Card.getCardByName(name).getPrice() > ProgramController.userInGame.getCoins()) {
+                buttonToUpdate.get(name).setDisable(true);
+                buttonToUpdate.get(name).setStyle("-fx-background-color: rgb(212, 29, 29,0.877);");
+            }
+            else {
+                buttonToUpdate.get(name).setDisable(false);
+                buttonToUpdate.get(name).setStyle("");
+            }
+        }
+    }
+
 
     private void addLabelsToVBox(String name, VBox vBox) {
         Label labelName = new Label(name);
@@ -67,6 +87,7 @@ public class ShopMenu implements Menuable {
             if (card.getName().equals(name))
                 countBought++;
         Label labelCount = new Label("Count : " + String.valueOf(countBought));
+        countToUpdate.put(name, labelCount);
         labelCount.setStyle("-fx-text-fill: white;");
         vBox.getChildren().add(labelCount);
         Label labelPrice = new Label("Price : " + String.valueOf(Card.getCardByName(name).getPrice()));
@@ -79,6 +100,12 @@ public class ShopMenu implements Menuable {
         ProgramController.userInGame.addCard(card);
         user.setCoins(user.getCoins() - (card != null ? card.getPrice() : 0));
         ((Label) ProgramController.currentScene.lookup("#coin")).setText(String.valueOf(ProgramController.userInGame.getCoins()));
+        int countBought = 0;
+        for (Card cardToCheck : ProgramController.userInGame.getCards())
+            if (cardToCheck.getName().equals(card.getName()))
+                countBought++;
+        countToUpdate.get(card.getName()).setText("Count : " + String.valueOf(countBought));
+        updateButtons();
     }
 
     public void back() throws IOException {
@@ -93,10 +120,11 @@ public class ShopMenu implements Menuable {
         stageCheat.show();
     }
 
-    public void increaseMoney() {
+    public void increaseMoney() throws IOException {
         stageCheat.close();
         int amount = Integer.parseInt(moneyCheat.getText());
         ProgramController.userInGame.setCoins(ProgramController.userInGame.getCoins() + amount);
-        ((Label) ProgramController.currentScene.lookup("#coin")).setText(String.valueOf(ProgramController.userInGame.getCoins()));
+        ((Label) ProgramController.currentScene.lookup("#coin")).setText("Coins : " + String.valueOf(ProgramController.userInGame.getCoins()));
+        updateButtons();
     }
 }
