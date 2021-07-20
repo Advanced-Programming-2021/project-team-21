3e77f06a72b494fbffc0e51f6fd53be5c40f41d1
farhadesmzdh <farhadesmzdh@gmail.com>
2021-07-20 14:15:46 -0700

    Shop Admin Panel

---
+++ Server/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module Yu.Gi.Oh {
+    requires javafx.graphics;
+    requires javafx.fxml;
+    requires com.google.gson;
+    requires cloning;
+    requires commons.math3;
+    requires javafx.controls;
+    exports view;
+}
